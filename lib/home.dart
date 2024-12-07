b6864318b4cec84a03e6de8a4599239f56faeab3
Rakin Reza <rakinr0715@gmail.com>
2024-12-07 12:35:39 -0500

    Finalized home.dart graphs

--- lib/home.dart
+++ lib/home.dart
@@ -11,9 +11,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final String _apiKey = 'ct7451pr01qr3sdtldr0ct7451pr01qr3sdtldrg'; 
-  final String _symbol = 'NVDA'; 
-  Map<String, dynamic>? _stockData;
+  final String _apiKey = 'ct7451pr01qr3sdtldr0ct7451pr01qr3sdtldrg'; // Replace with your Finnhub API Key
+  final List<String> _symbols = ['NVDA', 'TSLA', 'SPY', 'AAPL']; // Stock Symbols (Apple included)
+  Map<String, dynamic>? _nvidiaData;
+  Map<String, dynamic>? _teslaData;
+  Map<String, dynamic>? _sp500Data;
+  Map<String, dynamic>? _appleData;
   bool _isLoading = true;
 
   @override
@@ -27,20 +30,33 @@ class _HomeState extends State<Home> {
       _isLoading = true;
     });
 
-    final url = 'https://finnhub.io/api/v1/quote?symbol=$_symbol&token=$_apiKey';
-
     try {
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        setState(() {
-          _stockData = json.decode(response.body);
-          _isLoading = false;
-        });
-      } else {
-        throw Exception('Failed to load stock data');
+      for (String symbol in _symbols) {
+        final url = 'https://finnhub.io/api/v1/quote?symbol=$symbol&token=$_apiKey';
+        final response = await http.get(Uri.parse(url));
+        if (response.statusCode == 200) {
+          final data = json.decode(response.body);
+          setState(() {
+            if (symbol == 'NVDA') {
+              _nvidiaData = data;
+            } else if (symbol == 'TSLA') {
+              _teslaData = data;
+            } else if (symbol == 'SPY') {
+              _sp500Data = data;
+            } else if (symbol == 'AAPL') {
+              _appleData = data;
+            }
+          });
+        } else {
+          throw Exception('Failed to load data for $symbol');
+        }
       }
+
+      setState(() {
+        _isLoading = false;
+      });
     } catch (e) {
-      print('Error fetching stock data: $e');
+      print('Error fetching data: $e'); // Debug
       setState(() {
         _isLoading = false;
       });
@@ -58,90 +74,202 @@ class _HomeState extends State<Home> {
         ),
         backgroundColor: Colors.white,
         title: const Text("Home", style: TextStyle(color: Colors.black)),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Image.asset(
-              'Image/FTT.png',
-              height: 40,
-              width: 40,
-            ),
-          ),
-        ],
       ),
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
-          : _stockData != null
+          : (_nvidiaData != null &&
+                  _teslaData != null &&
+                  _sp500Data != null &&
+                  _appleData != null)
               ? Column(
                   children: [
-                    Container(height: 2, color: Colors.black, width: double.infinity),
-                    Expanded(
-                      child: BarChart(
-                        BarChartData(
-                          barGroups: _buildBarGroups(),
-                          titlesData: FlTitlesData(
-                            bottomTitles: AxisTitles(
-                              sideTitles: SideTitles(
-                                showTitles: true,
-                                getTitlesWidget: (value, meta) {
-                                  switch (value.toInt()) {
-                                    case 0:
-                                      return const Text('Low');
-                                    case 1:
-                                      return const Text('Current');
-                                    case 2:
-                                      return const Text('High');
-                                    case 3:
-                                      return const Text('Open');
-                                    default:
-                                      return const Text('');
-                                  }
-                                },
-                              ),
-                            ),
-                            leftTitles: AxisTitles(
-                              sideTitles: SideTitles(showTitles: true),
-                            ),
-                            topTitles: AxisTitles(
-                              sideTitles: SideTitles(showTitles: false),
-                            ),
-                            rightTitles: AxisTitles(
-                              sideTitles: SideTitles(showTitles: false),
-                            ),
-                          ),
-                          borderData: FlBorderData(show: false),
-                        ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        'Nvidia',
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                       ),
                     ),
-                    Container(height: 2, color: Colors.black, width: double.infinity),
+                    Expanded(
+                      flex: 1,
+                      child: Stack(
+                        alignment: Alignment.topCenter,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 16.0, right: 16.0, bottom: 32.0, top: 32.0),
+                            child: BarChart(
+                              BarChartData(
+                                minY: _calculateMinY(_nvidiaData!),
+                                maxY: _calculateMaxY(_nvidiaData!),
+                                titlesData: FlTitlesData(
+                                  leftTitles: AxisTitles(
+                                    sideTitles: SideTitles(showTitles: false),
+                                  ),
+                                  rightTitles: AxisTitles(
+                                    sideTitles: SideTitles(showTitles: false),
+                                  ),
+                                  topTitles: AxisTitles(
+                                    sideTitles: SideTitles(showTitles: false),
+                                  ),
+                                  bottomTitles: AxisTitles(
+                                    sideTitles: SideTitles(
+                                      showTitles: true,
+                                      getTitlesWidget: (value, meta) {
+                                        switch (value.toInt()) {
+                                          case 0:
+                                            return const Text('Low');
+                                          case 1:
+                                            return const Text('Current');
+                                          case 2:
+                                            return const Text('High');
+                                          case 3:
+                                            return const Text('Open');
+                                          default:
+                                            return const Text('');
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                borderData: FlBorderData(show: false),
+                                gridData: FlGridData(show: false),
+                                barGroups: _buildBarGroups(_nvidiaData!),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top: 40,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                _buildBarValue(_nvidiaData!['l'] as double, Colors.blue),
+                                _buildBarValue(_nvidiaData!['c'] as double, Colors.green),
+                                _buildBarValue(_nvidiaData!['h'] as double, Colors.red),
+                                _buildBarValue(_nvidiaData!['o'] as double, Colors.orange),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          _buildAdditionalItem(
+                              'Tesla', _teslaData!['c'].toString(), Colors.blue),
+                          _buildAdditionalItem(
+                              'S&P 500', _sp500Data!['c'].toString(), Colors.green),
+                          _buildAdditionalItem(
+                              'Apple', _appleData!['c'].toString(), Colors.red),
+                        ],
+                      ),
+                    ),
                   ],
                 )
               : const Center(child: Text('Failed to load data')),
     );
   }
 
-  List<BarChartGroupData> _buildBarGroups() {
-    final double lowPrice = _stockData!['l'] as double;
-    final double currentPrice = _stockData!['c'] as double;
-    final double highPrice = _stockData!['h'] as double;
-    final double openPrice = _stockData!['o'] as double;
+  double _calculateMinY(Map<String, dynamic> data) {
+    final lowPrice = data['l'] as double;
+    final currentPrice = data['c'] as double;
+    final highPrice = data['h'] as double;
+    final openPrice = data['o'] as double;
+
+    final minValue =
+        [lowPrice, currentPrice, highPrice, openPrice].reduce((a, b) => a < b ? a : b);
+    return minValue * 0.95;
+  }
+
+  double _calculateMaxY(Map<String, dynamic> data) {
+    final lowPrice = data['l'] as double;
+    final currentPrice = data['c'] as double;
+    final highPrice = data['h'] as double;
+    final openPrice = data['o'] as double;
+
+    final maxValue =
+        [lowPrice, currentPrice, highPrice, openPrice].reduce((a, b) => a > b ? a : b);
+    return maxValue * 1.05;
+  }
+
+  Widget _buildBarValue(double value, Color color) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Text(
+        value.toStringAsFixed(2),
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: color),
+      ),
+    );
+  }
+
+  Widget _buildAdditionalItem(String title, String value, Color color) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 8),
+        Icon(Icons.show_chart, size: 30, color: color),
+        SizedBox(height: 8),
+        Text(
+          value,
+          style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+        ),
+      ],
+    );
+  }
+
+  List<BarChartGroupData> _buildBarGroups(Map<String, dynamic> data) {
+    final double lowPrice = data['l'] as double;
+    final double currentPrice = data['c'] as double;
+    final double highPrice = data['h'] as double;
+    final double openPrice = data['o'] as double;
 
     return [
       BarChartGroupData(
         x: 0,
-        barRods: [BarChartRodData(toY: lowPrice, color: Colors.blue)],
+        barRods: [
+          BarChartRodData(
+            toY: lowPrice,
+            color: Colors.blue,
+            width: 12,
+          ),
+        ],
       ),
       BarChartGroupData(
         x: 1,
-        barRods: [BarChartRodData(toY: currentPrice, color: Colors.green)],
+        barRods: [
+          BarChartRodData(
+            toY: currentPrice,
+            color: Colors.green,
+            width: 12,
+          ),
+        ],
       ),
       BarChartGroupData(
         x: 2,
-        barRods: [BarChartRodData(toY: highPrice, color: Colors.red)],
+        barRods: [
+          BarChartRodData(
+            toY: highPrice,
+            color: Colors.red,
+            width: 12,
+          ),
+        ],
       ),
       BarChartGroupData(
         x: 3,
-        barRods: [BarChartRodData(toY: openPrice, color: Colors.orange)],
+        barRods: [
+          BarChartRodData(
+            toY: openPrice,
+            color: Colors.orange,
+            width: 12,
+          ),
+        ],
       ),
     ];
   }
